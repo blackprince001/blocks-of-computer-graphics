7caa2d98a305c959fa0af9bb605b381757e2ff0f
blackprince001 <appiahboaduprince@gmail.com>
2024-03-05 23:20:16 +0000

    Add lighting to cube rendering

--- render-cube/fragment_shader.fs
+++ render-cube/fragment_shader.fs
@@ -1,8 +1,26 @@
 #version 330 core
 
+in vec3 FragPos;
+
+uniform vec3 objectColor;
+uniform vec3 lightColor;
+uniform vec3 lightPos;
+
 out vec4 FragColor;
 
 void main()
 {
-    FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);
+    // Ambient lighting
+    float ambientStrength = 0.1;
+    vec3 ambient = ambientStrength * lightColor;
+
+    // Diffuse lighting
+    vec3 norm = normalize(FragPos);
+    vec3 lightDir = normalize(lightPos - FragPos);
+    float diff = max(dot(norm, lightDir), 0.0);
+    vec3 diffuse = diff * lightColor;
+
+    // Combine ambient and diffuse
+    vec3 result = (ambient + diffuse) * objectColor;
+    FragColor = vec4(result, 1.0);
 }
--- render-cube/vertex_shader.vs
+++ render-cube/vertex_shader.vs
@@ -1,9 +1,15 @@
-// shader versions mostly match the opengl version
 #version 330 core
 
-layout (location = 0) in vec2 aPos;
+layout (location = 0) in vec3 aPos;
+
+uniform mat4 model;
+uniform mat4 view;
+uniform mat4 projection;
+
+out vec3 FragPos;
 
 void main()
 {
-    gl_Position = vec4(aPos.x, aPos.y, 0, 1.0);
+    FragPos = vec3(model * vec4(aPos, 1.0));
+    gl_Position = projection * view * model * vec4(aPos, 1.0);
 }
